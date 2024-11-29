1d10dfd6f07b61f48534e955c966cc4e9dbb610d
Vexx3 <zekeloo2008@gmail.com>
2024-11-29 20:23:08 +0800

    feat: update user count retrieval to use approximateUserInstallCount for accuracy

--- events/ready.js
+++ events/ready.js
@@ -3,13 +3,12 @@ const { Events, ActivityType } = require("discord.js");
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+  async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
     const serverCount = client.guilds.cache.size;
-    const userCount = client.guilds.cache.reduce(
-      (acc, guild) => acc + guild.memberCount,
-      0
-    );
+    const userCount = await client.application
+      .fetch()
+      .then((app) => app.approximateUserInstallCount);
 
     const status = `Serving ${serverCount} servers and ${userCount} installed users`;
 
