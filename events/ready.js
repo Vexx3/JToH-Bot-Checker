1bc6f8cada500f4f60d5f231c4deacc719d11285
Vexx3 <zekeloo2008@gmail.com>
2024-11-29 20:13:58 +0800

    feat: update bot status to display server and user counts

--- events/ready.js
+++ events/ready.js
@@ -5,25 +5,15 @@ module.exports = {
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    const serverCount = client.guilds.cache.size;
+    const userCount = client.users.cache.size;
 
-    setInterval(() => {
-      const randomStatus = [
-        "Fetching JToH stats... Because your gameplay is a tragedy.",
-        "Tracking your JToH progress... Spoiler: You’re not improving.",
-        "Tracking your progress... Or the lack of it. Same thing.",
-        "Looking for towers you haven’t beaten... Which, judging by your track record, is all of them.",
-        "Fetching progress... Or, more accurately, the lack of it",
-        "Looking for the hardest towers... Let’s just hope you ever make it past 'Easy'.",
-      ];
+    const status = `Serving ${serverCount} servers and ${userCount} users`;
 
-      const status =
-        randomStatus[Math.floor(Math.random() * randomStatus.length)];
-
-      client.user.setActivity({
-        type: ActivityType.Custom,
-        name: "custom",
-        state: status,
-      });
-    }, 90000);
+    client.user.setActivity({
+      type: ActivityType.Custom,
+      name: "custom",
+      state: status,
+    });
   },
 };
