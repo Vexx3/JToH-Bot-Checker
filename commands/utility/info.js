13bbbeeed4be7c4400f1240334d4270dd65e3a11
Vexx3 <zekeloo2008@gmail.com>
2024-12-07 19:18:42 +0800

    feat: add info command to provide bot information and interaction links

---
+++ commands/utility/info.js
@@ -0,0 +1,89 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ApplicationIntegrationType,
+  InteractionContextType,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Get information about the bot")
+    .setIntegrationTypes([
+      ApplicationIntegrationType.GuildInstall,
+      ApplicationIntegrationType.UserInstall,
+    ])
+    .setContexts([
+      InteractionContextType.Guild,
+      InteractionContextType.BotDM,
+      InteractionContextType.PrivateChannel,
+    ]),
+  async execute(interaction) {
+    const userCount = await interaction.client.application
+      .fetch()
+      .then((app) => app.approximateUserInstallCount);
+
+    const embed = new EmbedBuilder()
+      .setTitle("Bot Information")
+      .setColor("#58b9ff")
+      .setDescription(
+        "A bot that provides various commands to interact with JToH game data, including checking tower completions, achievements, and more."
+      )
+      .addFields(
+        {
+          name: "Bot Name",
+          value: interaction.client.user.username,
+          inline: true,
+        },
+        {
+          name: "Servers",
+          value: interaction.client.guilds.cache.size.toString(),
+          inline: true,
+        },
+        {
+          name: "Installed Users",
+          value: userCount.toString(),
+          inline: true,
+        },
+        {
+          name: "Commands",
+          value: interaction.client.commands
+            .map((cmd) => `\`${cmd.data.name}\``)
+            .join(", "),
+        },
+        {
+          name: "Developer",
+          value: "_zylx",
+          inline: true,
+        },
+        {
+          name: "Library",
+          value: "[discord.js](https://discord.js.org/)",
+          inline: true,
+        }
+      )
+      .setThumbnail(interaction.client.user.displayAvatarURL());
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("Authorize TowerTracker")
+        .setStyle(ButtonStyle.Link)
+        .setURL(
+          "https://discord.com/oauth2/authorize?client_id=1306467343433596948"
+        ),
+      new ButtonBuilder()
+        .setLabel("Support Server")
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://discord.gg/wdP6GZB6KE"),
+      new ButtonBuilder()
+        .setLabel("JToH Game")
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://www.roblox.com/games/8562822414/")
+    );
+
+    await interaction.reply({ embeds: [embed], components: [row] });
+  },
+};
