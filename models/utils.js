c3542f439536ab1e27e2fa678870bccf21e3c15e
Vexx3 <zekeloo2008@gmail.com>
2024-12-09 19:29:21 +0800

    fix: update avatar image size in fetchRobloxAvatar function

--- models/utils.js
+++ models/utils.js
@@ -66,7 +66,7 @@ async function fetchRobloxAvatar(robloxId) {
       method: "GET",
       query: {
         userIds: robloxId,
-        size: "60x60",
+        size: "100x100",
         format: "Png",
         isCircular: false,
       },
