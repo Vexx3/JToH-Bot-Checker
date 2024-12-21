abd5bff75919382ae94b36d7d00c7cdc317ffece
Vexx3 <zekeloo2008@gmail.com>
2024-12-21 17:30:01 +0800

    fix: set default emoji for difficulty when not found in difficultyEmojis

--- commands/jtoh/unbeatentower.js
+++ commands/jtoh/unbeatentower.js
@@ -162,7 +162,7 @@ module.exports = {
                 )
                 .setValue(difficulty)
                 .setDescription(`Show only ${difficulty} towers`)
-                .setEmoji(difficultyEmojis[difficulty.toLowerCase()] || null)
+                .setEmoji(difficultyEmojis[difficulty.toLowerCase()] ||difficulty.toLowerCase())
                 .setDefault(selectedDifficulty === difficulty)
             ),
           ])
