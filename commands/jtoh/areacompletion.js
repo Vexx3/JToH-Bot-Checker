a6bceb2a9a0d76d099a1b149d6a5b9d68dce9007
Vexx3 <zekeloo2008@gmail.com>
2024-12-24 01:01:18 +0800

    fix: update area completion embed to use description for completion list

--- commands/jtoh/areacompletion.js
+++ commands/jtoh/areacompletion.js
@@ -76,7 +76,7 @@ module.exports = {
       .setTitle(`Tower completion in area ${areaCode} for ${username}`)
       .setColor("#58b9ff")
       .setThumbnail(avatarUrl)
-      .addFields({ name: "Completion", value: completionList });
+      .setDescription(`**Completion**\n${completionList}`);
 
     await interaction.editReply({ embeds: [embed] });
   },
